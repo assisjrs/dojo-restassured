6138697c4f9dacfa227dd14c638d8f2976b9944a
Assis Junior <assisjrs@gmail.com>
2022-08-08 15:48:56 -0300

    criação dos projetos clean e do projeto e2e

---
+++ springboot-clean-project/src/main/java/com/assisjrs/sample/dojorestassured/DojoRestassuredApplication.java
@@ -0,0 +1,13 @@
+package com.assisjrs.sample.dojorestassured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoRestassuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoRestassuredApplication.class, args);
+	}
+
+}
---
+++ springboot-clean-project/src/main/java/com/assisjrs/sample/dojorestassured/controller/ClienteController.java
@@ -0,0 +1,10 @@
+package com.assisjrs.sample.dojorestassured.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+}
